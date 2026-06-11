f2226be6634c0fb245568b35cc19844ad1fd3fc2
navyashreesr <navyashreesr.1805@gmail.com>
2026-06-11 20:51:32 +0530

    first commit - task done e1

---
+++ gitdemo2/cal.py
@@ -0,0 +1,12 @@
+a=2
+b=3
+
+sum=+b
+diff=b-a
+mul=a*b
+div=a/b
+
+print(sum)
+print(diff)
+print(mul)
+print(div)
---
+++ gitdemo2/class.py
@@ -0,0 +1,7 @@
+class add:
+ def sum():
+    a=2
+    b=3
+    return a+b
+b=add()
+print(b.add())
